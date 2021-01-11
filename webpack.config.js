b46073c4891f0248034ae558ff9978afe059b7b5
Yan Paulo <yanpaulo@hotmail.com>
2021-01-11 09:11:20 -0300

    Suporte a pasta raíz, pasta de entrada e de destino; Melhorias no suporte ao VS

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,56 @@
 var path = require('path');
 var glob = require("glob");
+const webpack = require('webpack');
 
-var sourcePath = "./src/";
-var outPath = "./dist/";
+var rootPath = "./wwwroot/"
+var sourcePath = rootPath + "js/src/";
+var outPath = rootPath + "js/dist/";
 
 module.exports = {
     mode: "development",
-    devtool: "inline-source-map",
-    context: __dirname,
-    entry: () =>
-    {
-        
-        var files = glob.sync(sourcePath + "/**/_*.ts");
-        
-        var entries = files.reduce((o, v) => { 
-            var item = {};
-            item[v] = v;
-            return item;
-          }, {});
-          console.log(entries);
+    devtool: false,
+    entry: () => {
+        var files = glob.sync(sourcePath + "**/_*.ts");
+        var entries = files.reduce((o, v) => {
+            var key = v.slice(sourcePath.length);
+            o[key] = v;
+            return o;
+        }, {});
+        console.log("Entries:");
+        console.log(entries);
         return entries;
     },
     output: {
         filename: (pathData) => {
             var filename = '' + pathData.chunk.name;
-            filename = filename.slice(sourcePath.length);
             filename = filename.slice(0, filename.lastIndexOf('.ts'));
             filename = filename + '.js';
-            console.log(filename);
             return filename;
         },
-      path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, outPath)
     },
     resolve: {
-      // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: [".ts", ".tsx", ".js"]
+        extensions: [".ts", ".tsx", ".js"],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
     },
     module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: "ts-loader" }
-      ]
-    }
-  }
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            moduleFilenameTemplate: info => {
+                if (info.resourcePath.startsWith(rootPath)) {
+                    var path = info.resourcePath.slice(rootPath.length);
+                    return path;
+                }
+                return info.resourcePath;
+            },
+            sourceRoot: '/',
+        })
+    ]
+
+}
